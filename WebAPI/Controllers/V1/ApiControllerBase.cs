811d760dec68917158bb8450615b62fcfc7a3a7e
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-12 16:50:18 +0200

    Added MediatR

---
+++ WebAPI/Controllers/V1/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI.Controllers.V1
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+        private ISender _mediator;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>();
+    }
+}
---
+++ WebAPI/Controllers/V1/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers.V1
+{
+    public class UsersController : ApiControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+    }
+}
