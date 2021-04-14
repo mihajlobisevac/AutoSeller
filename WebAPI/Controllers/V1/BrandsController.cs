e47fbe3e60d94d4b34e7e66545a14d74cc5db635
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-15 01:42:51 +0200

    Added 'BrandsController' and 'GetAll' action

---
+++ WebAPI/Controllers/V1/BrandsController.cs
@@ -0,0 +1,16 @@
+﻿using Application.V1.Brands.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers.V1
+{
+    public class BrandsController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<GetBrands.Response>> GetAll()
+        {
+            return await Mediator.Send(new GetBrands.Query());
+        }
+    }
+}
