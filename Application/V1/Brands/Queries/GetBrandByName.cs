e87bf83638901bef73e3d3eabdbf9f8265b3cd34
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-15 01:58:35 +0200

    Added 'GetBrandByName'
    'GetBrandByName' query returns a brand based on 'BrandName' property. Also added a controller action which sends this request. This action returns 200 or 404 based on the result.

---
+++ Application/V1/Brands/Queries/GetBrandByName.cs
@@ -0,0 +1,48 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Mappings;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.V1.Brands.Queries
+{
+    public static class GetBrandByName
+    {
+        public record Query(string BrandName) : IRequest<Response>;
+
+        public class Handler : IRequestHandler<Query, Response>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var brand = await _context.Brands
+                    .Include(x => x.Models)
+                    .FirstOrDefaultAsync(x => x.Name == request.BrandName, cancellationToken);
+
+                if (brand is null) return null;
+
+                return _mapper.Map<Response>(brand);
+            }
+        }
+
+        public record Response : IMapFrom<Brand>
+        {
+            public int Id { get; init; }
+            public string Name { get; init; }
+            public string Country { get; init; }
+            public ICollection<Model> Models { get; init; }
+        }
+    }
+}
--- WebAPI/Controllers/V1/BrandsController.cs
+++ WebAPI/Controllers/V1/BrandsController.cs
@@ -1,6 +1,6 @@
 ﻿using Application.V1.Brands.Queries;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers.V1
@@ -8,9 +8,23 @@ namespace WebAPI.Controllers.V1
     public class BrandsController : ApiControllerBase
     {
         [HttpGet]
-        public async Task<IEnumerable<GetBrands.Response>> GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return await Mediator.Send(new GetBrands.Query());
+            var brands = await Mediator.Send(new GetBrands.Query());
+
+            return brands is not null || brands.Any()
+                ? Ok(brands)
+                : NotFound();
+        }
+
+        [HttpGet("by-name/{brandName}")]
+        public async Task<IActionResult> GetByName(string brandName)
+        {
+            var brand = await Mediator.Send(new GetBrandByName.Query(brandName));
+
+            return brand is not null
+                ? Ok(brand)
+                : NotFound();
         }
     }
 }
