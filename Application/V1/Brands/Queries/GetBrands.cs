c0a1fac8061a0c67946138b0f89fef97b32e9c63
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-15 01:39:48 +0200

    Added 'GetBrands' request
    'GetBrands' gets a list of all brands and maps each to a response dto.

--- Application/V1/Brands/Queries/GetBrands.cs
+++ Application/V1/Brands/Queries/GetBrands.cs
@@ -1,6 +1,11 @@
 ﻿using Application.Common.Interfaces;
+using Application.Common.Mappings;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,27 +13,30 @@ namespace Application.V1.Brands.Queries
 {
     public static class GetBrands
     {
-        public record Query : IRequest<Response>;
+        public record Query : IRequest<IEnumerable<Response>>;
 
-        public class Handler : IRequestHandler<Query, Response>
+        public class Handler : IRequestHandler<Query, IEnumerable<Response>>
         {
             private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(IApplicationDbContext context)
+            public Handler(IApplicationDbContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
-            public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<IEnumerable<Response>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var brands = await _context.Brands
-                    .ToListAsync();
+                    .ProjectTo<Response>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
 
-                return null;
+                return brands;
             }
         }
 
-        public record Response
+        public record Response : IMapFrom<Brand>
         {
             public int Id { get; init; }
             public string Name { get; init; }
