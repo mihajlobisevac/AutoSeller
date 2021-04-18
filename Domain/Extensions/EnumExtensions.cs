9dc51fc06934dacd146fabaf9290e825d1372cbb
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-19 00:42:22 +0200

    Added 'ToEnumArray<T>' extension method
    This extension method takes an array of strings and converts them into an array of enums. T represents the type of the enum.

--- Domain/Extensions/EnumExtensions.cs
+++ Domain/Extensions/EnumExtensions.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Domain.Extensions
 {
     public static class EnumExtensions
     {
-        public static T ToEnum<T>(this string position) => (T)Enum.Parse(typeof(T), position, false);
+        public static T ToEnum<T>(this string enumName) => (T)Enum.Parse(typeof(T), enumName, false);
+
+        public static T[] ToEnumArray<T>(this string[] array)
+        {
+            List<T> enumList = new();
+
+            foreach (var item in array)
+            {
+                T enumType = (T)Enum.Parse(typeof(T), item, false);
+                enumList.Add(enumType);
+            }
+
+            return enumList.ToArray();
+        }
     }
 }
