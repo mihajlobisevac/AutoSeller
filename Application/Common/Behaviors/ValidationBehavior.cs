f72149bec81f0fb4358dbd10eace3d5efc9ffc36
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-20 03:29:37 +0200

    Validation behavior setup

---
+++ Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Models;
+using Application.Common.Validation;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TResponse : CQRSResponse, new()
+    {
+        private readonly IValidationHandler<TRequest> _validationHandler;
+
+        public ValidationBehavior() { }
+
+        public ValidationBehavior(IValidationHandler<TRequest> validationHandler)
+        {
+            _validationHandler = validationHandler;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validationHandler == null) return await next();
+
+            var result = await _validationHandler.Validate(request);
+
+            if (result.IsSuccessful == false)
+            {
+                //potential log: result.Error
+                string error = $"One or more validations failed";
+                return new TResponse { IsSuccessful = false, ErrorMessage = error };
+            }
+
+            return await next();
+        }
+    }
+}
--- Application/Common/Validation/ValidationResult.cs
+++ Application/Common/Validation/ValidationResult.cs
@@ -3,9 +3,9 @@
     public record ValidationResult
     {
         public bool IsSuccessful { get; set; } = true;
-        public string ErrorMessage { get; init; }
+        public string Error { get; init; }
 
         public static ValidationResult Success => new();
-        public static ValidationResult Fail(string error) => new() { IsSuccessful = false, ErrorMessage = error };
+        public static ValidationResult Fail(string error) => new() { IsSuccessful = false, Error = error };
     }
 }
