e326807a72405271a942bb7e846b688db53d04ae
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-21 00:45:36 +0200

    Added validator for editing brand

---
+++ Application/V1/Brands/Commands/EditBrandValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Validation;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Application.V1.Brands.Commands
+{
+    public class EditBrandValidator : IValidationHandler<EditBrand.Command>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public EditBrandValidator(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ValidationResult> Validate(EditBrand.Command request)
+        {
+            var brand = await _context.Brands.FirstOrDefaultAsync(x => x.Name == request.Name);
+
+            if (brand is not null) return ValidationResult.Fail("Name already in use.");
+
+            return ValidationResult.Success;
+        }
+    }
+}
