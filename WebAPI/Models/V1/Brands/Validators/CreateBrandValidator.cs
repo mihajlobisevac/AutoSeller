415b4a1d56f506bb829a707e50e879fd8704808a
Mihajlo Bisevac <kajler11@gmail.com>
2021-04-26 01:46:44 +0200

    Api validators for creating and editing brands
    Commands for creating and editing brands have property 'BrandName' which is required to be not empty.

---
+++ WebAPI/Models/V1/Brands/Validators/CreateBrandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Application.V1.Brands.Commands;
+using FluentValidation;
+
+namespace WebAPI.Models.V1.Brands.Validators
+{
+    public class CreateBrandValidator : AbstractValidator<CreateBrand.Command>
+    {
+        public CreateBrandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Brand name can't be empty.");
+        }
+    }
+}
