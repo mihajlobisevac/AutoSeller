e2e00f40a9fb8d18e3a7b35056148c99ac377e3c
Mihajlo Bisevac <kajler11@gmail.com>
2021-05-11 15:27:51 +0200

    Added 'LoginUser' and 'RefreshUserToken' requests
    'LoginUser' takes email and password and returns jwt token + refresh token if successful.
    'RefreshUserToken' takes jwt token and refresh token and returns a new pair of same if successful

---
+++ Application/V1/Users/Commands/RefreshUserToken.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.V1.Users.Commands
+{
+    public static class RefreshUserToken
+    {
+        public record Command(string JwtToken, string RefreshToken) : IRequest<CQRSResponse>;
+
+        public class Handler : IRequestHandler<Command, CQRSResponse>
+        {
+            private readonly IAuthService _authService;
+
+            public Handler(IAuthService authService)
+            {
+                _authService = authService;
+            }
+
+            public async Task<CQRSResponse> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var result = await _authService.ValidateAndCreateTokensAsync(
+                    request.JwtToken,
+                    request.RefreshToken);
+
+                if (result is null) return new CQRSResponse("Failed validating tokens");
+
+                return result;
+            }
+        }
+    }
+}
