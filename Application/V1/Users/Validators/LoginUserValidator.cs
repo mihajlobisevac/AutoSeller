bfd5450609836b0c0710cb3393f34c7fbdf690ab
Mihajlo Bisevac <kajler11@gmail.com>
2021-05-12 19:35:36 +0200

    Added 'LoginUserValidator'
    Moved validation checks from 'LoginUser' command into 'LoginUserValidator' for separation of concerns purposes and consistency

---
+++ Application/V1/Users/Validators/LoginUserValidator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Validation;
+using Application.V1.Users.Commands;
+using System.Threading.Tasks;
+
+namespace Application.V1.Users.Validators
+{
+    public class LoginUserValidator : IValidationHandler<LoginUser.Command>
+    {
+        private readonly IIdentityService _identityService;
+
+        public LoginUserValidator(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<ValidationResult> Validate(LoginUser.Command request)
+        {
+            var validCredentials = await _identityService.CheckCredentialsAsync(request.Email, request.Password);
+            if (validCredentials == false) return ValidationResult.Fail("Invalid credentials");
+
+            return ValidationResult.Success;
+        }
+    }
+}
