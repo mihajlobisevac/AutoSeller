6b18fd7f4bc0163997cc4ecb712fb0852b8ae27c
Mihajlo Bisevac <kajler11@gmail.com>
2021-05-12 19:47:36 +0200

    Added extension methods for returning 'CQRSResponse' depending on 'IdentityResult' succeeded state

--- Infrastructure/Identity/IdentityExtensions.cs
+++ Infrastructure/Identity/IdentityExtensions.cs
@@ -6,11 +6,26 @@ namespace Infrastructure.Identity
 {
     public static class IdentityExtensions
     {
-        public static Result ToApplicationResult(this IdentityResult result)
+        public static CQRSResponse ToCQRSResponse(this IdentityResult identityResult, IdentityRole role)
         {
-            return result.Succeeded
-                ? Result.Success()
-                : Result.Failure(result.Errors.Select(e => e.Description).ToArray());
+            if (identityResult.Succeeded == false)
+            {
+                var errors = identityResult.Errors.Select(x => x.Description).ToArray();
+                return CQRSResponse.Fail(errors);
+            }
+
+            return IdentityRoleResponse.Success(role);
+        }
+
+        public static CQRSResponse ToCQRSResponse(this IdentityResult identityResult, ApplicationUser user)
+        {
+            if (identityResult.Succeeded == false)
+            {
+                var errors = identityResult.Errors.Select(x => x.Description).ToArray();
+                return CQRSResponse.Fail(errors);
+            }
+
+            return IdentityUserResponse.Success(user);
         }
     }
 }
