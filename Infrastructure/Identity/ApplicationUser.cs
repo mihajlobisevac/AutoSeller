f13bdab192d6ab9235446625278cbda504561c4c
Mihajlo Bisevac <kajler11@gmail.com>
2021-05-12 19:49:21 +0200

    Added 'IdentityUserResponse'
    Inherits 'CQRSResponse' and has additional properties: UserId & Email. Contains 'Success' extension method which takes 'ApplicationUser' as parameter

--- Infrastructure/Identity/ApplicationUser.cs
+++ Infrastructure/Identity/ApplicationUser.cs
@@ -4,5 +4,14 @@ namespace Infrastructure.Identity
 {
     public class ApplicationUser : IdentityUser
     {
+        public ApplicationUser(string email, string username)
+        {
+            Email = email;
+            UserName = username;
+        }
+
+        public ApplicationUser()
+        {
+        }
     }
 }
