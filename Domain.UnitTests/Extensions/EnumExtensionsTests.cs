9f4e41e15c08edec98bff837296b52513e6db3bc
Mihajlo Bisevac <kajler11@gmail.com>
2021-05-14 17:34:34 +0200

    Project setup

---
+++ Domain.UnitTests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+using Domain.Extensions;
+using System.Linq;
+using Xunit;
+
+namespace Domain.UnitTests.Extensions
+{
+    public class EnumExtensionsTests
+    {
+        
+    }
+}
