be2f5c5d7d0dc13fa6a27fb75bb983baa2ca3842
Mihajlo Bisevac <kajler11@gmail.com>
2021-05-14 16:40:34 +0200

    Project setup

---
+++ Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Mappings;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+
+    }
+}
